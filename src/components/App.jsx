353c0b0d134b522caf5de2cfbb5073fc900d0c0e
MoidHasanBeig <mhbdgr8@gmail.com>
2019-12-07 06:23:41 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Mindmap from "./Mindmap"
+
+function App() {
+  return <Mindmap />
+}
+
+export default App;
---
+++ src/components/Mindmap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Mindmap() {
+  return (
+    <div>
+      <div class="circular-container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Mindmap;
